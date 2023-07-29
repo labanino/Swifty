d85f738f8ba0134a1b89029bcb162de80580a14e
Alex Labanino <ghostrider6610@gmail.com>
2023-07-29 19:20:46 -0400

    Initial Commit

---
+++ Swifty/SwiftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyApp.swift
+//  Swifty
+//
+//  Created by Alex Labanino on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

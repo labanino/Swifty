7ba8f2a2d0b1d632f2ab073e8b7663e01e4162b7
Alex Labanino <ghostrider6610@gmail.com>
2023-08-04 22:58:34 -0400

    Building a SwiftUI little app fir iOS 17

--- Swifty/ContentView.swift
+++ Swifty/ContentView.swift
@@ -9,13 +9,98 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image(.image1)
+                .resizable()
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                .frame(height: 300)
+                .cornerRadius(20)
+            
+            VStack(alignment: .center) {
+                
+                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry.")
+                    .font(.subheadline)
+                HStack(spacing: 12.0) {
+                    VStack(alignment: .leading) {
+                        Text("Size")
+                            .foregroundColor(.secondary)
+                        Text("1024x1024")
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    
+                    Divider()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Type")
+                            .foregroundColor(.secondary)
+                        Text("Upscale")
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    
+                    Divider()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Date")
+                            .foregroundColor(.secondary)
+                        Text("Today 5:19")
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                }
+                .frame(height: 44.0)
+                
+                HStack {
+                    HStack {
+                        Image(systemName: "ellipsis")
+                        Divider()
+                        Image(systemName: "sparkle.magnifyingglass")
+                        Divider()
+                        Image(systemName: "face.smiling")
+                    }
+                    .padding()
+                    .frame(height: 44)
+                    .overlay(
+                        UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(topLeading: 0,
+                                                bottomLeading: 20,
+                                                bottomTrailing: 0,
+                                                topTrailing: 20))
+                            .strokeBorder(linearGradietn)
+                    )
+                    .offset(x: -20, y: 20)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "square.and.arrow.down")
+                        .padding()
+                        .frame(height: 44)
+                        .overlay(
+                            UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(topLeading: 20,
+                                                    bottomLeading: 0,
+                                                    bottomTrailing: 20,
+                                                    topTrailing: 0))
+                                .strokeBorder(linearGradietn)
+                        )
+                        .offset(x: 20, y: 20)
+                }
+            }
+            .padding(20.0)
+            .background(.regularMaterial)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .strokeBorder(linearGradietn)
+            )
+            .cornerRadius(20.0)
+            .padding(20)
+            .offset(y: 80)
         }
-        .padding()
+        .frame(maxWidth: 400)
+        .padding(20)
+        .dynamicTypeSize(.xSmall ... .xxLarge)
+    }
+    var linearGradietn: LinearGradient {
+        LinearGradient(colors: [.clear, .primary.opacity(0.3), .clear], startPoint: .topLeading, endPoint: .bottomTrailing)
     }
 }
 
